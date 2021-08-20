917dee0d2c69e3c14feeb9f78f366c99c8a04e95
ThiagoIvens <thiagorech2121@gmail.com>
2021-08-19 21:56:38 -0300

    update

--- app/models.py
+++ app/models.py
@@ -1,26 +1,29 @@
 from django.db import models
-from model_utils.models import TimeStampedModel
 
 # Create your models here.
-class Portifolio(TimeStampedModel):
+class Portifolio(models.Model):
     name = models.CharField(max_length=255, blank=False)
     image = models.ImageField(upload_to="portifolio/%Y/%m/%d", blank=False)
+    created = models.CharField(max_length=255)
+    modified = models.CharField(max_length=255)
 
     objects = models.Manager()
     class Meta:
         ordering = ("name",)
-    
+
     def __str__(self):
         return self.name
 
-class Projetos(TimeStampedModel):
+class Projetos(models.Model):
     name = models.CharField(max_length=255, blank=False)
     image = models.ImageField(upload_to="projetos/%Y/%m/%d", blank=False)
+    created = models.CharField(max_length=255)
+    modified = models.CharField(max_length=255)
 
     objects = models.Manager()
     class Meta:
         ordering = ("name",)
         verbose_name_plural = "Projetos"
-    
+
     def __str__(self):
         return self.name
